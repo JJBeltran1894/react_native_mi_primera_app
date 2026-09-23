de39a710e0252c03e9f6d922a5435a0cc545ae73
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-09-23 02:30:53 -0500

    JJBA: Archivo principal del app - Muestra Header y EmployeeList con relacion en flex de 0.3 y 0.7 resoectivamente + Estilos creados con IA - Taller 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ProfileCard from "./src/components/ProfileCard";
+import EmployeeList from "./src/components/EmployeeList";
+import CompanyHeader from "./src/components/CompanyHeader";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar style="light" backgroundColor="#2173F3" />
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.appContainer}>
+          <View style={styles.headerContainer}>
+            <CompanyHeader
+              nombreEmpresa="Vitalis"
+              slogan="Con cariño te cuidamos"
+              logotipo={require("./src/assets/images/logotipo.jpg")}
+            />
+          </View>
+          <View style={styles.listContainer}>
+            <EmployeeList />
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  headerContainer: {
+    flex: 0.3,
+    backgroundColor: "#faf6f6",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  listContainer: {
+    flex: 0.7,
+    backgroundColor: "#ced1db",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#11214d",
+  },
+
+  appContainer: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#ffff",
   },
 });
